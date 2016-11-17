101725df4cf1b2db63607169d37a09f64d7df0b5
Eric Eaglstun <postpostmodern@gmail.com>
2016-11-17 12:52:38 -0600

    fix file name

--- index.php
+++ index.php
@@ -7,6 +7,6 @@ if( is_admin() )
 
 require __DIR__.'/functions.php';
 require __DIR__.'/theme.php';
-require __DIR__.'/lib/class-dbug.php';
+require __DIR__.'/lib/WP_Dbug/Dbug.php';
 
 Dbug::setup();
